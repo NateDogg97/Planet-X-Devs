9017009cbf8f3aca2588654cb19656aa99174464
Nathaniel <natmays97@gmail.com>
2025-07-12 11:05:45 -0500

    add white label page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -30,6 +30,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/white-label-web-development`,
+      lastModified: currentDate,
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/style-guide`,
       lastModified: new Date('2025-06-20'),
       changeFrequency: 'yearly',
