2ddd38bd4fe545aa9b5af764aab6a08c28f75955
Nathaniel <natmays97@gmail.com>
2025-07-13 18:23:45 -0500

    update Google Tag Manager and remove Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/navigation/Header";
 import { PerformanceProvider } from "@/components/providers/PerformanceProvider";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -169,27 +169,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager - Script 1 */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WCRL2WXZ');
-          `}
-        </Script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-50V3N0YZKK"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-50V3N0YZKK');
-            `,
-          }}
-        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
@@ -203,15 +182,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <PerformanceProvider enabled={true}>
-          {/* Google Tag Manager - noscript */}
-          <noscript>
-            <iframe 
-              src="https://www.googletagmanager.com/ns.html?id=GTM-WCRL2WXZ"
-              height="0" 
-              width="0" 
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
+          <GoogleTagManager gtmId="GTM-WCRL2WXZ"/>
           <Header />
           <main className="pt-24">{children}</main>
         </PerformanceProvider>
